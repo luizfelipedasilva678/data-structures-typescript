dd257f071ef94965a76b121ab0dad353ef9769cc
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-03-15 22:35:43 -0300

    BinarySearchTree min, max and others methods

--- src/tree/binary-search-tree.ts
+++ src/tree/binary-search-tree.ts
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-param-reassign */
 import {
   Compare,
@@ -42,6 +43,76 @@ export default class BinarySearchTree {
       this.inOrderTraverseNode(node.right, callback);
     }
   }
+
+  preOrderTraverse(callback: any) {
+    this.preOrderTraverseNode(this.root, callback);
+  }
+
+  preOrderTraverseNode(node: any, callback: any) {
+    if (node != null) {
+      callback(node.key);
+      this.preOrderTraverseNode(node.left, callback);
+      this.preOrderTraverseNode(node.right, callback);
+    }
+  }
+
+  postOrderTraverse(callback: any) {
+    this.postOrderTraverseNode(this.root, callback);
+  }
+
+  postOrderTraverseNode(node: any, callback: any) {
+    if (node != null) {
+      this.postOrderTraverseNode(node.left, callback);
+      this.postOrderTraverseNode(node.right, callback);
+      callback(node.key);
+    }
+  }
+
+  min() {
+    this.minNode(this.root);
+  }
+
+  minNode(node: any) {
+    let current = node;
+    while (current != null && current.left != null) {
+      current = current.left;
+    }
+
+    return current;
+  }
+
+  max() {
+    this.minNode(this.root);
+  }
+
+  maxNode(node: any) {
+    let current = node;
+    while (current != null && current.right != null) {
+      current = current.right;
+    }
+
+    return current;
+  }
+
+  search(key: any) {
+    return this.searchNode(this.root, key);
+  }
+
+  searchNode(node: any, key: any): any {
+    if (node == null) {
+      return false;
+    }
+
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      return this.searchNode(node.left, key);
+    }
+
+    if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      return this.searchNode(node.right, key);
+    }
+
+    return true;
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -61,3 +132,10 @@ tree.insert(15);
 tree.insert(6);
 const printfNode = (value: any) => console.log(value);
 tree.inOrderTraverse(printfNode);
+console.log('-----');
+tree.preOrderTraverse(printfNode);
+console.log('-----');
+tree.postOrderTraverse(printfNode);
+console.log('-----');
+console.log(tree.min());
+console.log(tree.search(3));
