4ca1bddcb3858211111daef09559cde45f96d935
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-03-31 22:15:47 -0300

    Lcs

---
+++ src/knapSack/knap-sack.js
@@ -0,0 +1,36 @@
+function knapSack(capacity, weights, values, n) {
+  const kS = [];
+  for (let i = 0; i <= n; i++) {
+    kS[i] = [];
+  }
+
+  for (let i = 0; i <= n; i++) {
+    for (let w = 0; w <= capacity; w++) {
+      if (i === 0 || w === 0) {
+        kS[i][w] = 0;
+      } else if (weights[i - 1] <= w) {
+        const a = values[i - 1] + kS[i - 1][w - weights[i - 1]];
+        const b = kS[i - 1][w];
+        kS[i][w] = a > b ? a : b;
+      } else {
+        kS[i][w] = kS[i - 1][w];
+      }
+    }
+  }
+
+  findValues(n, capacity, kS, weights, values);
+  return kS[n][capacity];
+}
+
+function findValues(n, capacity, kS, weights, values) {
+  let i = n;
+  let k = capacity;
+  while (i > 0 && k > 0) {
+    if (kS[i][k] !== kS[i - 1][k]) {
+      i--;
+      k -= kS[i][k];
+    } else {
+      i--;
+    }
+  }
+}
---
+++ src/lcs/lcs.js
@@ -0,0 +1,21 @@
+function lcs(wordX, wordY) {
+  const m = wordX.length;
+  const n = wordY.length;
+  const l = [];
+
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= n; j++) {
+      if (i === 0 || j === 0) {
+        l[i][j] = 0;
+      } else if (wordX[i - 1] === wordY[j - 1]) {
+        l[i][j] = l[i - 1][j - 1] + 1;
+      } else {
+        const a = l[i - 1][j];
+        const b = l[i][j - 1];
+        l[i][j] = a > b ? a : b;
+      }
+    }
+  }
+
+  return l[m][n];
+}
